e4b2e116c3a54f3d0a43d85bd683c778d111220d
nidhisn <nidhi24sn@gmail.com>
2024-11-29 23:12:49 +0530

    first commit

---
+++ BrandPage1Project1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSetion from "./Components/Hero";
+import Navigation from "./Components/Navigation";
+
+const App=()=>{
+return(
+    <div>
+      <Navigation /> 
+      <HeroSetion /> 
+    </div>
+);
+};
+
+export default App;
